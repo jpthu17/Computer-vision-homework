c94c540e8a9bd88ec7c0705a27b768a662402520
jpthu17 <1003901354@qq.com>
2021-10-21 20:14:58 +0800

    update

--- 第五次作业/test.py
+++ 第五次作业/test.py
@@ -27,20 +27,39 @@ def get_fb2(x, w1, b1, w2, b2, y):
     return 2*(get_y(x, w1, b1, w2, b2)-y)
 
 
+class Net(torch.nn.Module):
+    def __init__(self, n_feature, n_hidden, n_output):
+        super(Net, self).__init__()
+        self.prediction = torch.nn.Sequential(
+                            torch.nn.Linear(n_feature, n_hidden, bias=True),
+                            torch.nn.Sigmoid(),
+                            torch.nn.Linear(n_hidden, n_output, bias=True)
+                            )
+
+    def forward(self, x):
+        x = self.prediction(x)
+        return x
+
+
 if __name__ == '__main__':
+
+    net = Net(n_feature=1, n_hidden=20, n_output=1)     # define the network
+    print(net)  # net architecture
+
     torch.manual_seed(0)
-    x = torch.randn(10, 4, requires_grad=True)
-    w1 = torch.randn(4, 4, requires_grad=True)
-    b1 = torch.randn(10, 4, requires_grad=True)
-    w2 = torch.randn(4, 4, requires_grad=True)
-    b2 = torch.randn(10, 4, requires_grad=True)
-    y = torch.randn(10, 4, requires_grad=True)
+    x = torch.randn(1, 1, requires_grad=True)
+    y = torch.randn(1, 1, requires_grad=True)
+    prediction = net(x)     # input x and predict based on x
+    loss = ((prediction - y)*(prediction - y)).sum()     # must be (1. nn output, 2. target)
 
-    loss = ((get_y(x, w1, b1, w2, b2) - y) * (get_y(x, w1, b1, w2, b2) - y)).sum()
+    loss.backward()         # backpropagation, compute gradients
 
-    loss.backward()
+    w1 = net.prediction[0].weight.t()
+    w2 = net.prediction[2].weight.t()
+    b1 = net.prediction[0].bias
+    b2 = net.prediction[2].bias
 
-    print("w1.grad:\n pytorch:\n {}\n ours:\n {}".format(w1.grad, get_fw1(x, w1, b1, w2, b2, y)))
-    print("w2.grad:\n pytorch:\n {}\n ours:\n {}".format(w2.grad, get_fw2(x, w1, b1, w2, b2, y)))
-    print("b1.grad:\n pytorch:\n {}\n ours:\n {}".format(b1.grad, get_fb1(x, w1, b1, w2, b2, y)))
-    print("b2.grad:\n pytorch:\n {}\n ours:\n {}".format(b2.grad, get_fb2(x, w1, b1, w2, b2, y)))
+    print("w1.grad:\n pytorch:\n {}\n ours:\n {}".format(net.prediction[0].weight.grad.t(), get_fw1(x, w1, b1, w2, b2, y)))
+    print("w2.grad:\n pytorch:\n {}\n ours:\n {}".format(net.prediction[2].weight.grad.t(), get_fw2(x, w1, b1, w2, b2, y)))
+    print("b1.grad:\n pytorch:\n {}\n ours:\n {}".format(net.prediction[0].bias.grad, get_fb1(x, w1, b1, w2, b2, y)))
+    print("b2.grad:\n pytorch:\n {}\n ours:\n {}".format(net.prediction[2].bias.grad, get_fb2(x, w1, b1, w2, b2, y)))
